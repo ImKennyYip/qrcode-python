155e1b3526b414a3741ecbb743dbfce6ecd9220c
Kenny Yip Coding <78777681+ImKennyYip@users.noreply.github.com>
2025-11-30 11:44:54 +0000

    Update generate-qrcode.py

--- generate-qrcode.py
+++ generate-qrcode.py
@@ -9,7 +9,8 @@ img.save(img_file)
 
 qr = qrcode.QRCode()
 qr.add_data(url)
-# image = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+# img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
 img = qr.make_image()
 
 img.save(img_file)
+
