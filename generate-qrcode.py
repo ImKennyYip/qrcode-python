f9e0120a3981238e565a56665c7533602a5ed63c
Kenny Yip Coding <78777681+ImKennyYip@users.noreply.github.com>
2025-11-30 11:44:44 +0000

    Update generate-qrcode.py

--- generate-qrcode.py
+++ generate-qrcode.py
@@ -2,14 +2,14 @@ import qrcode
 
 # url = "kennyyipcoding.com"
 url = input("Enter a URL: ").strip() #removes leading/trailing whitespaces
-image_file = "./qrcode.png"
+img_file = "./qrcode.png"
 
 img = qrcode.make(url)
-img.save(image_file)
-
+img.save(img_file)
 
 qr = qrcode.QRCode()
 qr.add_data(url)
 # image = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
 img = qr.make_image()
-img.save(image_file)
+
+img.save(img_file)
